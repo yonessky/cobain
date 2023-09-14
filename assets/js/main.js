44c62e7059d316bf3b19275f13898cb7604e1ef2
Yonesky Simamora <yonesskysimamora@gmail.com>
2023-09-14 15:53:56 +0700

    kantor

--- assets/js/main.js
+++ assets/js/main.js
@@ -319,6 +319,7 @@ function loadMore(loadMoreBtn, loadedItem) {
 }
 
 loadMore('.loadMoreproject', '.project-hidden > .project__card');
+loadMore('.loadMoremall', '.project-hiddenmall > .project__card');
 
 // let loadMoreBtn = document.querySelector('#load-more');
 // let currentItem = 3;
@@ -346,59 +347,3 @@ loadMore('.loadMoreproject', '.project-hidden > .project__card');
 //   });
   
 // })
-
-/*=============== PARTICLES ===============*/
-var particles = Particles.init({
-	selector: '.background',
-  color: '#ffffff'
-});
-
-Particles.
-init
-({
-  
-// normal options
-//   selector: 
-// '.background'
-// ,
-//   maxParticles: 
-// 450
-// ,
-  
-// options for breakpoints
-  responsive: [
-    {
-      breakpoint: 
-768
-,
-      options: {
-        maxParticles: 
-100
-,
-        connectParticles: 
-false
-      }
-    }, {
-      breakpoint: 
-425
-,
-      options: {
-        maxParticles: 
-50
-,
-        connectParticles: 
-true
-      }
-    }, {
-      breakpoint: 
-320
-,
-      options: {
-        maxParticles: 
-0
- 
-// disables particles.js
-      }
-    }
-  ]
-});
